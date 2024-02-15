6261f6865852ff31a1a0c32764fda5e86872c194
Daniel-Laz <160122188+Daniel-Laz@users.noreply.github.com>
2024-02-15 19:33:45 +0100

    aggiunta opzione di cancellazione a Main.java
    correzioni e modifiche di alcuni System.out.println; 
    risoluzione bug tramite ricerca online;
    aggiunta opzione di cancellazione;

--- Main.java
+++ Main.java
@@ -4,14 +4,13 @@ public class Main
 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int posizione=0;
+        int posizione;
         int temp;
         int temp2;
         int numero;
         String istruzione;
         int[] numeri=new int[100];
         Random generatore = new Random();
-
 
         System.out.println("quanti valori vuoi inserire?");
         int lunghezza=in.nextInt();
@@ -25,17 +24,25 @@ public class Main
         }
         System.out.println("\n----------");
 
-
+        // MENU
         do{
             System.out.println("Vuoi inserire un numero? (digita inserisci)");
-            System.out.println("Vuoi rimuovere un numero? (digita rimuovi)");
-            System.out.println("Vuoi visualizzare a video l'array (digita visualizza)");
+            System.out.println("Vuoi inserire un numero casuale? (digita random)");
+            System.out.println("Vuoi rimuovere un numero(posizione)? (digita rimuovi posizione)");
+            System.out.println("Vuoi rimuovere un numero(occorrenze)? (digita rimuovi occorrenze)");
+            System.out.println("Vuoi visualizzare a video l'array? (digita visualizza)");
             System.out.println("Termina il programma (digita stop)");
+            System.out.println("----------");
+
+            //IL DO WHILE SALTA DEI CICLI SENZA LEGGERE L'ISTRUZIONE; QUESTA é UNA SOLUZIONE CHE HO TROVATO ONLINE
+            in.skip("\\R?");
             istruzione=in.nextLine();
-            istruzione=in.nextLine();
+
+
+
             // ISTRUZIONE INSERISCI
             if(istruzione.equalsIgnoreCase("inserisci")){
-                System.out.println("inserisci la posizione");
+                System.out.println("inserisci la posizione del numero da inserire");
                 posizione=in.nextInt()-1;
                 System.out.println("inserisci il numero da inserire");
                 numero=in.nextInt();
@@ -43,11 +50,9 @@ public class Main
                 numeri[posizione]=numero;
                 lunghezza++;
                 for (int i=posizione;i<numeri.length-1;i++) {
-
                     temp2=numeri[i+1];
                     numeri[i+1]=temp;
                     temp=temp2;
-
                 }
             }
 
@@ -67,32 +72,41 @@ public class Main
                 numeri[posizione]=generatore.nextInt(50);
                 lunghezza++;
                 for (int i=posizione;i<numeri.length-1;i++) {
-
                     temp2=numeri[i+1];
                     numeri[i+1]=temp;
                     temp=temp2;
-
                 }
             }
 
-            // ISTRUZIONE CANCELLA
-            if (istruzione.equalsIgnoreCase("rimuovi")){
+            // ISTRUZIONE CANCELLA (POSIZIONE)
+            if (istruzione.equalsIgnoreCase("rimuovi posizione")){
                 System.out.println("inserisci la posizione del numero che vuoi rimuovere");
                 posizione=in.nextInt()-1;
                 lunghezza--;
                 temp=numeri[numeri.length-1];
                 for (int i=numeri.length-1;i>posizione;i--){
-
                     temp2=numeri[i-1];
                     numeri[i-1]=temp;
                     temp=temp2;
-
-
                 }
             }
 
+            // ISTRUZIONE CANCELLA (OCCORRENZE DI UN ELEMENTO INSERITO)
+            if (istruzione.equalsIgnoreCase("rimuovi occorrenze")){
+                System.out.println("quale numero vuoi eliminare?");
+                int nDaRimuovere=in.nextInt();
+                for (int i=0;i<lunghezza;i++){
+                    if (numeri[i]==nDaRimuovere){
+                        lunghezza--;
+                        for (int j=i; j<numeri.length-1;j++){
+                            numeri[j]=numeri[j+1];
+                        }
+                    }
+                }
+            }
+            System.out.println("----------");
 
 
-         }while(!istruzione.equalsIgnoreCase("stop"));
+        }while(!istruzione.equalsIgnoreCase("stop"));
     }
 }
